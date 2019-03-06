ae8e28d9aa455d61f1dfa9bd813d208bc233a590
Diogo Brown <diogo_brown95@hotmail.com>
2019-03-06 09:59:40 -0300

    Acrescentada uma mensagem de bom dia

--- teste1/Program.cs
+++ teste1/Program.cs
@@ -11,6 +11,7 @@ namespace teste1
         static void Main(string[] args)
         {
             Console.WriteLine("Olá mundo");
+            Console.WriteLine("Bom dia");
             Console.ReadLine();
         }
     }
